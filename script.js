4089529c792844021ce0595cfd88cf5525524562
Turriis <arturocecena@hotmail.com>
2023-04-05 20:02:42 -0600

    Sitio web de hora del día terminado.

--- script.js
+++ script.js
@@ -5,20 +5,25 @@ function carregar() {
     var hora = data.getHours();
     console.log(img)
 
+
+
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if(hora >= 0 && hora < 12){
         //BoM DIA
         img.src = 'css/images/morning.jpg'
+        document.body.style.backgroundColor = "rgb(174, 174, 218)"
 
     }
     else if(hora >= 12 && hora < 19){
         //BOA TARDE
         img.src = 'css/images/afternoon.jpg'
+        document.body.style.backgroundColor =" rgb(199, 173, 29)"
     }
     else{
         //BOA NOITE
         img.src = 'css/images/night.jpg'
+        document.body.style.backgroundColor = "rgb(38, 6, 51)"
     }
 
 
